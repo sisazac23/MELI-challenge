7c6ded7f4bbad2e8baab818c10a1be8d7ce76078
sisazac23 <sisazac23@gmail.com>
2025-10-19 20:06:14 -0500

    pasan 3 test, resuelto test api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,36 +1,25 @@
-
-from fastapi.testclient import TestClient
 from mlops_housing.train import train_and_register
 from mlops_housing.config import DEFAULT_DATA_PATH
-from mlops_housing.api.app import app  
-
+from mlops_housing.api.app import app, load_model  
+from fastapi.testclient import TestClient
 
 def test_predict_endpoint():
-    # Entrenamos ANTES de levantar la app
+
+    #  Entrenar antes de levantar la app
     train_and_register(str(DEFAULT_DATA_PATH), tag="test_api")
 
-    # Ahora cargamos la app (evento startup encontrará el modelo activo)
-    client = TestClient(app)
+    # Forzar recarga del modelo en la app 
+    load_model()
 
-    payload = {
-        "CRIM": 0.1,
-        "ZN": 18,
-        "INDUS": 2.3,
-        "CHAS": 0,
-        "NOX": 0.5,
-        "RM": 6.2,
-        "AGE": 45,
-        "DIS": 4.2,
-        "RAD": 1,
-        "TAX": 300,
-        "PTRATIO": 15,
-        "B": 390,
-        "LSTAT": 5.0
-    }
-
-    response = client.post("/predict", json=payload)
-
-    assert response.status_code == 200
-    data = response.json()
-    assert "predicted_price" in data
-    assert isinstance(data["predicted_price"], float)
+    # 3) Usar context manager para respetar lifespan/startup/shutdown
+    with TestClient(app) as client:
+        payload = {
+            "CRIM": 0.1, "ZN": 18, "INDUS": 2.3, "CHAS": 0, "NOX": 0.5,
+            "RM": 6.2, "AGE": 45, "DIS": 4.2, "RAD": 1, "TAX": 300,
+            "PTRATIO": 15, "B": 390, "LSTAT": 5.0
+        }
+        resp = client.post("/predict", json=payload)
+        assert resp.status_code == 200, resp.text
+        data = resp.json()
+        assert "predicted_price" in data
+        assert isinstance(data["predicted_price"], float)
